a80d2b7c192b6fdfe8a70f241af36d553885028c
Ionică Bizău <bizauionica@gmail.com>
2015-09-15 16:31:11 +0300

    Forward the cli arguments

--- downloads/import.sh
+++ downloads/import.sh
@@ -8,7 +8,7 @@ for sour in */ ; do
         for repo in */ ; do
             cd $repo
             echo ">> $repo"
-            git-stats-importer
+            git-stats-importer $*
             cd ..
         done
         cd ..
